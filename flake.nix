a92316810f4ea9372f566a1ea685b1c80b552c02
Matt Carrier <icecreammatt@gmail.com>
2023-02-25 14:18:31 -0800

    build: use clang instead of gcc to build

--- flake.nix
+++ flake.nix
@@ -49,9 +49,9 @@
       shellHook = let
         icon = "f121";
       in ''
-        #export COMPILER="clang++"
-        export COMPILER="g++"
         export PS1="$(echo -e '\u${icon}') {\[$(tput sgr0)\]\[\033[38;5;228m\]\w\[$(tput sgr0)\]\[\033[38;5;15m\]} (${name}) \\$ \[$(tput sgr0)\]"
+        export COMPILER="clang++"
+        #export COMPILER="g++"
         make
       '';
     };
