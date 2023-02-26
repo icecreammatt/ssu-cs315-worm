2ae9879d4b93c6ee6ae2baec4bdb445de210b08a
Matt Carrier <icecreammatt@gmail.com>
2023-02-26 10:57:39 -0800

    fix: remove unused code and add comments

--- flake.nix
+++ flake.nix
@@ -19,20 +19,10 @@
     # Add the system/architecture you would like to support here. Note that not
     # all packages in the official nixpkgs support all platforms.
     "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"
-  ] (system: let
-    pkgs = import nixpkgs {
-      inherit system;
-
-      # Add overlays here if you need to override the nixpkgs
-      # official packages.
-      overlays = [];
-      
-      # Uncomment this if you need unfree software (e.g. cuda) for
-      # your project.
-      #
-      # config.allowUnfree = true;
-    };
+  ] (system: let pkgs = import nixpkgs { inherit system; };
   in {
+
+    # This block here is used when running `nix develop`
     devShells.default = pkgs.mkShell rec {
       # Update the name to something that suites your project.
       name = "worm";
@@ -42,11 +32,12 @@
         ncurses6
       ];
 
-      # Setting up the environment variables you need during
-      # development.
+      # Setting up the environment variables you need during development.
+
       # Todo figure out why I can't use clang on Asahi but can on Darwin
       # Use "clang++" for most systems but OSX Asahi requires g++ for some reason or a runtime error occurs
       shellHook = let
+        # This is for an icon that is used below for the command line input below
         icon = "f121";
       in ''
         export PS1="$(echo -e '\u${icon}') {\[$(tput sgr0)\]\[\033[38;5;228m\]\w\[$(tput sgr0)\]\[\033[38;5;15m\]} (${name}) \\$ \[$(tput sgr0)\]"
@@ -56,21 +47,21 @@
       '';
     };
 
+    # This is used when running `nix build`
     packages.default = pkgs.llvmPackages_14.stdenv.mkDerivation rec {
       pname = "worm";
       version = "0.1.1";
   
       src = self;
 
-      nativeBuildInputs = [ ];
-
       buildInputs = [ pkgs.ncurses6 ];
 
       buildPhase = "COMPILER='clang++' make";
 
-      installPhase = "mkdir -p $out/bin; install -t $out/bin worm";
-
-      cmakeFlags = [ ];
+      installPhase = ''
+        mkdir -p $out/bin; 
+        install -t $out/bin worm
+      '';
 
       meta = with inputs.utils.lib; {
         homepage = "https://github.com/icecreammatt/ssu-cs315-worm";
