bb5bd551ee2dd49b5ec4c6a03a7e40eb9bf713fd
Matt Carrier <icecreammatt@gmail.com>
2023-03-25 23:55:43 -0700

    build: do not auto build on nix develop add envrc

--- flake.nix
+++ flake.nix
@@ -43,8 +43,6 @@
         export PS1="$(echo -e '\u${icon}') {\[$(tput sgr0)\]\[\033[38;5;228m\]\w\[$(tput sgr0)\]\[\033[38;5;15m\]} (${name}) \\$ \[$(tput sgr0)\]"
         export COMPILER="clang++"
         #export COMPILER="g++"
-        cd src
-        make
       '';
     };
 
